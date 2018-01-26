d057ba77eb313fb390760a20093a16e5a9326bcb
太田　智彬 <tomoaki.ota14@gmail.com>
2018-01-27 01:04:33 +0900

    init

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,8 +3,7 @@ require "calculator"
 RSpec.describe Calculator do
   describe '#add' do
     it 'returns the sum of its arguments' do
-      # expect(Calculator.new.add(1, 2)).to eq(3)
-      expect(Calculator.new.add(1, 2)).to eq(4)
+      expect(Calculator.new.add(1, 2)).to eq(3)
     end
   end
 end
